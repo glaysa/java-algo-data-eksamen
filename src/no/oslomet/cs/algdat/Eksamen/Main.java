8c82ff7c25c19c67e2171a2a9ea8a2f369d7930a
Glaysa <glaysa.df@gmail.com>
2020-10-21 16:15:21 +0200

    Oppgave 03: Hvis gitt array er slik [10,2,10,10,10] gir den feil resultat men passerte alle oppgave 03 testene

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -4,15 +4,12 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
-        Integer[] a = {10,10,10,10};
         EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
-        for (int verdi : a) tre.leggInn(verdi);
-        System.out.println(tre.antall()); // Utskrift: 10
-        System.out.println(tre.antall(5)); // Utskrift: 0
-        System.out.println(tre.antall(4)); // Utskrift: 3
-        System.out.println(tre.antall(7)); // Utskrift: 2
-        System.out.println(tre.antall(10));// Utskrift: 1
+        int[] a = {6, 14, 1, 8, 12, 3, 7, 9, 11, 13, 2, 5, 4};
+        int[] b = {10,10,10,10};
 
+        //for (int verdi : a) tre.leggInn(verdi);
+        for (int verdi : b) tre.leggInn(verdi);
         String r = tre.toStringPostOrder();
         System.out.println(r);
     }
