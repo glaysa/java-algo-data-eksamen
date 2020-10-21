8e74d9d32627f0129ea2ba8e0e532ee582a4e2a9
Glaysa <glaysa.df@gmail.com>
2020-10-21 11:17:58 +0200

    Oppgave 01: Ferdig
    Passerte alle oppgave 01 testene

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -1,7 +1,12 @@
 package no.oslomet.cs.algdat.Eksamen;
 
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args){
-
+        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+        EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre.leggInn(verdi);
+        System.out.println(tre.antall());
     }
 }
