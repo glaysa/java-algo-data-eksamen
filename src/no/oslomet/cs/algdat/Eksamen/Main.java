3c98806075ecc40876012780db9f7a5ca70b3ccf
Glaysa <glaysa.df@gmail.com>
2020-10-21 11:21:09 +0200

    Oppgave 02: Kjørte metoden i main og gir feil resultat

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -4,9 +4,9 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
-        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+        Integer[] a = {4,4,7,2,9,5,10,8,1,3,6};
         EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
         for (int verdi : a) tre.leggInn(verdi);
-        System.out.println(tre.antall());
+        System.out.println(tre.antall(4));
     }
 }
