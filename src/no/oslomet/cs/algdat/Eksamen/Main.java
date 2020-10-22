0041fa965fcba2abebd3598f3a77f7efeab5e595
Glaysa <glaysa.df@gmail.com>
2020-10-22 14:52:00 +0200

    Oppgave 04: Ferdig
    Passerte alle oppgave 04 testene

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -8,20 +8,5 @@ public class Main {
         int[] a = {10, 6, 14, 1, 8, 12, 3, 7, 9, 11, 13, 2, 5, 4};
         int[] b = {10,10,2,10,10};
         for (int verdi : a) tre.leggInn(verdi);
-
-        Oppgave<Integer> print = new Oppgave<Integer>() {
-            @Override
-            public void utførOppgave(Integer integer) {
-                System.out.print(integer + " ");
-            }
-        };
-
-        System.out.println("Postorden(oppgave)");
-        tre.postorden(print);
-
-        System.out.println();
-
-        System.out.println("Postorden(rot,oppgave)");
-        tre.postordenRecursive(print);
     }
 }
